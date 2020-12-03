720a6647f9df913cf631451e74dc90db0d43dcb9
aditya26-dev <dewaa.kadek26@gmail.com>
2020-12-03 21:59:06 +0800

    Initial Commit

---
+++ Cooklah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cooklah
+//
+//  Created by I Dewa Agung Ary Aditya Wibawa on 03/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
